eef9b0ed62313928736f526989ed38e1e3287648
Survivor <survivor@xaker.ru>
2019-12-27 16:02:24 +0400

    Create DamageTypes.cs
    Created damage types

---
+++ DamageTypes.cs
@@ -0,0 +1,23 @@
+namespace Survaxe
+{
+    public enum DamageTypes
+    {
+        Falling = 0,
+        Explosion = 1,
+        Prop = 2,
+        Fire = 3,
+        Food = 4,
+        Water = 5,
+        Bleeding = 6,
+        Poisoning = 7,
+        Radiation = 8,
+        Suicide = 8,
+        Entity = 9,
+        Range = 10,
+        Melee = 11,
+        Electric = 12,
+        Null = 13,
+        Freezing = 14,
+        Bite = 15
+    }
+}
